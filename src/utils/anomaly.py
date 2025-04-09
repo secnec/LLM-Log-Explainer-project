18f582ab10fe853594e5255f174000afa5f08646
secnec <43522516+secnec@users.noreply.github.com>
2025-04-09 11:11:25 +0300

    Revert "BGL anomaly"

--- src/utils/anomaly.py
+++
@@ -1,7 +0,0 @@
-import dspy
-from typing import Literal
-
-class AnomalyLabeler(dspy.Signature):
-    """Label the anomaly based on the text."""
-    text: str = dspy.InputField()
-    label: Literal['application', 'authentication', 'io', 'memory', 'network', 'other', ] = dspy.OutputField()
