88b5195fc2bcae6053c6fc955664be2a99729a97
Antti Kosonen <antti.kosonen@protonmail.com>
2025-04-10 13:18:14 +0300

    BGL added to FCS

--- src/filecontextselection.py
+++ src/filecontextselection.py
@@ -1,6 +1,7 @@
 
 import polars as pl
 from loglead.loaders.raw import RawLoader
+from loglead.loaders.bgl import BGLLoader
 from loglead.enhancers import EventLogEnhancer
 from loglead.anomaly_detection import AnomalyDetector
 
@@ -32,6 +33,12 @@ class FileContextSelection:
         ])
 
         return df
+
+    def load_BGL_file(self) -> pl.DataFrame:
+        bgl_loader = BGLLoader(filename=self.file_path)
+        df = bgl_loader.execute()
+        return df
+    
     
     def enhance(self, df: pl.DataFrame) -> pl.DataFrame:
         df = df.with_columns([
@@ -72,18 +79,57 @@ class FileContextSelection:
         df = anomaly_detector.predict()
 
         return df
+    
+
+    def get_IF_anomalies(self, df: pl.DataFrame):
+        anomaly_detector = AnomalyDetector(
+            item_list_col="e_words",
+            numeric_cols=["e_words_len", "e_trigrams_len", "e_chars_len", "e_lines_len", "e_event_id_len"],
+            store_scores=True,
+            print_scores=False,
+            auc_roc=False
+        )
+
+        anomaly_detector.test_train_split(df, test_frac=0.90, shuffle=True)
+        anomaly_detector.train_IsolationForest(n_estimators=100, contamination="auto")
+        df = anomaly_detector.predict()
+
+        return df
+
+    def get_token_count(self, df: pl.DataFrame):
+        return df.height
+
 
     def get_context(self):
+        
         if self.filetype == "LO2":
             df = self.load_LO2_file()
+        elif self.filetype == "BGL":
+            df = self.load_BGL_file()
         else:
             return None
-
         df = self.enhance(df)
 
         if self.anomaly_detection_method == "LOF":
             df = self.get_LOF_anomalies(df)
-            df = df.rename({"pred_ano": "context"})
-            return df
+
+        if self.anomaly_detection_method == "IF":
+            df = self.get_IF_anomalies(df)
         else:
             return None
+        
+        df = df.with_row_count(name="idx")
+        df = df.sort("pred_ano", descending=True)
+        while True:
+            limit = 100000
+            tokens = self.get_token_count(df)
+            if tokens <= limit:
+                df = df.sort("idx", descending=False)
+                df = df.drop("idx")
+                return df
+            n_total = df.height
+            n_drop = int(n_total * (1-limit/tokens))
+            df = df.slice(0, n_total - n_drop)
+        return df
+
+
